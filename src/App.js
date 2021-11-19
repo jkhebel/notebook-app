8a9d716d6198994d076813021f2c2ea1658bbe92
Jai <jaihebel@gmail.com>
2021-11-18 22:38:32 -0800

    moved html to app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import Header from "./components/Header";
+import React, { useEffect } from 'react';
+
 
 function check_file_APIs() { // function to check if browser can handle local file API
   if (window.File && window.FileReader && window.FileList && window.Blob && window.showDirectoryPicker) {
@@ -9,30 +11,34 @@ function check_file_APIs() { // function to check if browser can handle local fi
   }
 }
 
-
 check_file_APIs()
 
-let directory; // window.showDirectoryPicker must be attached to a user event
-document.getElementById('addToFolder').addEventListener('click', async () => {
-    try {
-        directory = await window.showDirectoryPicker();
-
-        for await (const entry of directory.values()) {
-            console.log(entry);
-            let item = document.createElement('li');
-            item.innerHTML = `<strong>${entry.name}</strong> - ${entry.kind}`;
-            document.getElementById('fileList').append(item);
-        }
-    } catch(e) {
-        console.log(e);
-    }
-});
-
-
 function App() {
+  useEffect(() => {
+    let directory; // window.showDirectoryPicker must be attached to a user event
+    document.getElementById('addToFolder').addEventListener('click', async () => {
+        try {
+            directory = await window.showDirectoryPicker();
+            let fileList = document.getElementById('fileList');
+            fileList.innerHTML = '';
+            for await (const entry of directory.values()) {
+                console.log(entry);
+                let item = document.createElement('li');
+                item.innerHTML = `<strong>${entry.name}</strong> - ${entry.kind}`;
+                fileList.append(item);
+            }
+        } catch(e) {
+            console.log(e);
+        }
+    });
+  });
   return (
     <div className="container">
       <Header title="Notebook"/>
+      <button id='addToFolder'>
+        Choose Notebook Folder
+      </button>
+      <ul id="fileList"></ul>
     </div>
   );
 }

cff5550c637ec8f0b9b30c39f0eea874ee4c681f
Jai <jaihebel@gmail.com>
2021-11-18 21:15:42 -0800

    trying to load local files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import Header from "./components/Header";
 
+const fs = require('fs')
+
+const dir = '.'
+const files = fs.readdirSync(dir)
+
+for (const file of files) {
+  console.log(file)
+}
+
+
 function App() {
   return (
     <div className="container">

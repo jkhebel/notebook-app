c637e34c7293214fc9db747848dda7718b8484ac
Jai <jaihebel@gmail.com>
2021-11-18 22:02:53 -0800

    simple button to show directory contents

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
 import Header from "./components/Header";
 
-const fs = require('fs')
-
-const dir = '.'
-const files = fs.readdirSync(dir)
-
-for (const file of files) {
-  console.log(file)
+function check_file_APIs() { // function to check if browser can handle local file API
+  if (window.File && window.FileReader && window.FileList && window.Blob && window.showDirectoryPicker) {
+    // Great success! All the File APIs are supported.
+    console.log("File APIs are supported!")
+  } else {
+    console.log('The File APIs are not fully supported in this browser.');
+  }
 }
+
+
+check_file_APIs()
+
+let directory; // window.showDirectoryPicker must be attached to a user event
+document.getElementById('addToFolder').addEventListener('click', async () => {
+    try {
+        directory = await window.showDirectoryPicker();
+
+        for await (const entry of directory.values()) {
+            console.log(entry);
+            let item = document.createElement('li');
+            item.innerHTML = `<strong>${entry.name}</strong> - ${entry.kind}`;
+            document.getElementById('fileList').append(item);
+        }
+    } catch(e) {
+        console.log(e);
+    }
+});
 
 
 function App() {
